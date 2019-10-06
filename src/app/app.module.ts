bb2c47fb40f31782564bd72db6db956212c91cb4
khailegend <nhanchikhai@gmail.com>
2019-10-07 04:44:32 +0700

    add project Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import {RouterModule} from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    RegisterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
+  
+    RouterModule.forRoot([ 
+      { path: '' , component:HomeComponent},
+      { path: 'home' , component:HomeComponent},
+      { path: 'dangnhap', 	component:SignInComponent },
+      { path: 'dangky', 	component:RegisterComponent },
+      { path: 'lienhe', 	component:ContactComponent }
+    ]),
     AppRoutingModule
   ],
   providers: [],
